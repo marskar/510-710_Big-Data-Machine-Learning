f799e78230f5fbb176d59f0ef6d3a8a746a8a85d
Martin Skarzynski <marskar@gmail.com>
2018-05-06 18:11:10 -0400

    refactored all models, next up learning curves

--- BDML.py
+++ BDML.py
@@ -90,7 +90,7 @@ def run_pipe2(name, clf):
 
 
 # 10-fold cross_validation
-def get_score(name, clf):
+def get_cvscore(name, clf):
     scores = cross_val_score(clf, X, y, cv=10)
     print(f"Algo: {name:<20s} and Score: {np.mean(scores):0.4f}")
     return name, np.mean(scores)
@@ -127,107 +127,127 @@ pd.DataFrame(l_modelstats, columns=("Algorithm",
 
 # Employ GridSearchCV() to find the optimal parameters for SVC's kernel = 'rbf',
 def gs_svc(c_param, gamma_param):
+    print('Grid search for SVC')
     pipe = Pipeline([('scl', StandardScaler()), ('clf', SVC())])
     pipe.fit(X_train, y_train)  # <- note that pipe line inherits clf methods
     print(pipe.score(X_test, y_test))  # notably .fit(), .predict() and .score()
     params = {'clf__kernel': ['rbf'], 'clf__C': c_param, 'clf__gamma': gamma_param}
     grid = GridSearchCV(estimator=pipe, param_grid=params, cv=3)
     grid.fit(X_train, y_train)
-    print(grid.best_estimator_)
-    print(' ')
-    print(grid.best_score_)
-    print(' ')
-    print(grid.score(X_test, y_test))
-    print(' ')
-    print(grid.best_params_)
-    # nested cross-validation csv
-    gs = GridSearchCV(estimator=pipe, param_grid=params, scoring='accuracy', cv=2)
-    scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
-    print(f'CV accuracy: {np.mean(scores):.3f} +/- {np.std(scores):.3f}')
+    print('Best estimator:', grid.best_estimator_)
+    print('Best training score:', grid.best_score_)
+    print('Best testing score:', grid.score(X_test, y_test))
+    print('Best parameter(s):', grid.best_params_)
+    return grid.best_score_, grid.score(X_test, y_test), grid.best_params_
 
 
 c = [0.01, 1, 10, 100]
 gamma = [0.001, 0.01, 0.1, 1.0, 100.0]
-gs_svc(c, gamma)
+best_svc = gs_svc(c, gamma)
+
+
+# nested cross-validation svc
+def get_score(pipe, params):
+    gs = GridSearchCV(estimator=pipe, param_grid=params, scoring='accuracy', cv=2)
+    scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
+    print(f'CV accuracy: {np.mean(scores):.3f} +/- {np.std(scores):.3f}')
+    return np.mean(scores), np.std(scores)
+
+
+pipeline = Pipeline([('scl', StandardScaler()), ('clf', SVC())])
+parameters = {'clf__kernel': ['rbf'], 'clf__C': c, 'clf__gamma': gamma}
+svc_mean = get_score(pipeline, parameters)
+
 
 # Employ GridSearchCV() to find the optimal parameters for DecisionTreeClassifier
-c = [5, 10, 15, 20, 25, 30]
-pipe = Pipeline([('scl', StandardScaler()), ('clf', DecisionTreeClassifier())])
-params = {'clf__max_depth': c}
-score_types = ['accuracy', 'roc_auc', 'f1']
-print('Grid search for DecisionTree')
-print(' ')
-for scoret in score_types:
-    grid = GridSearchCV(estimator=pipe, param_grid=params, scoring=scoret, cv=3)
+def gs_dtc(based_on):
+    print('Grid search for DecisionTree')
+    c_list = [5, 10, 15, 20, 25, 30]
+    pipe = Pipeline([('scl', StandardScaler()), ('clf', DecisionTreeClassifier())])
+    params = {'clf__max_depth': c_list}
+    grid = GridSearchCV(estimator=pipe, param_grid=params, scoring=based_on, cv=3)
     grid.fit(X_train, y_train)
-    print("Score based on", scoret)
-    print('best training score', grid.best_score_)
-    print('best testing score', grid.score(X_test, y_test))
-    print('best params', grid.best_params_)
-    print(' ')
-# nested cross-validation DecisionTree
-gs = GridSearchCV(estimator=pipe, param_grid=params, scoring='accuracy', cv=2)
+    print("Score based on:", based_on)
+    print('Best training score:', grid.best_score_)
+    print('Best testing score:', grid.score(X_test, y_test))
+    print('Best parameter(s):', grid.best_params_)
+    return based_on, grid.best_score_, grid.score(X_test, y_test), grid.best_params_
 
-scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
-print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+score_types = ['accuracy', 'roc_auc', 'f1']
+t_dtc = tuple(map(gs_dtc, score_types))
+
+pipeline = Pipeline([('scl', StandardScaler()), ('clf', DecisionTreeClassifier())])
+parameters = {'clf__max_depth': [5, 10, 15, 20, 25, 30]}
+dtc_mean = get_score(pipeline, parameters)
 
 # Employ GridSearchCV() to find the optimal parameters for RandomForestClassifier
-clf = RandomForestClassifier()
 c = [5, 7, 9, 11]
 n_estimators = [90, 95, 100, 105]
 max_features = [1, 3, 5, 7]
 
-pipe = Pipeline([('scl', StandardScaler()),
-                 ('clf', RandomForestClassifier(max_depth=5, n_estimators=100, max_features=1))])
 
-params = {'clf__max_depth': c,
-          'clf__n_estimators': n_estimators,
-          'clf__max_features': max_features
-          }
+def gs_rfc(based_on):
+    print('Grid search for Random Forest')
+    pipe = Pipeline([('scl', StandardScaler()), ('clf', RandomForestClassifier())])
+    params = {'clf__max_depth': [5, 7, 9, 11],
+              'clf__n_estimators': [90, 95, 100, 105],
+              'clf__max_features': [1, 3, 5, 7]
+              }
+    grid = GridSearchCV(estimator=pipe, param_grid=params, scoring=based_on, cv=3)
+    grid.fit(X_train, y_train)
+    print("Score based on:", based_on)
+    print('Best training score:', grid.best_score_)
+    print('Best testing score:', grid.score(X_test, y_test))
+    print('Best parameter(s):', grid.best_params_)
+    return based_on, grid.best_score_, grid.score(X_test, y_test), grid.best_params_
+
 
 score_types = ['accuracy', 'roc_auc', 'f1']
-for scoret in score_types:
-    grid = GridSearchCV(estimator=pipe, param_grid=params, scoring=scoret, cv=3)
-    grid.fit(X_train, y_train)
-    print("Score based on", scoret)
-    print('best training score', grid.best_score_)
-    print('best testing score', grid.score(X_test, y_test))
-    print('best params', grid.best_params_)
-    print(' ')
-# nested cross-validation RandomForest
-gs = GridSearchCV(estimator=pipe, param_grid=params, scoring='accuracy', cv=2)
+t_rfc = tuple(map(gs_rfc, score_types))
 
-scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
-print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+pipeline = Pipeline([('scl', StandardScaler()), ('clf', RandomForestClassifier())])
+parameters = {'clf__max_depth': [5, 7, 9, 11],
+              'clf__n_estimators': [90, 95, 100, 105],
+              'clf__max_features': [1, 3, 5, 7]
+              }
+rfc_mean = get_score(pipeline, parameters)
+
 
 # Employ GridSearchCV() to find the optimal parameters for KNN
-clf = KNeighborsClassifier()
-n_neighbors = [5, 8, 10, 12, 15]
-
-pipe = Pipeline([('scl', StandardScaler()), ('clf', KNeighborsClassifier(5))])
-
-params = {'clf__n_neighbors': n_neighbors,
-          }
-score_types = ['accuracy', 'roc_auc', 'f1']
-print('Grid search for knn')
-print(' ')
-scoret: str
-for scoret in score_types:
-    grid = GridSearchCV(estimator=pipe, param_grid=params, scoring=scoret, cv=3)
+def gs_knc():
+    print('Grid search for KNN')
+    pipe = Pipeline([('scl', StandardScaler()), ('clf', KNeighborsClassifier())])
+    params = {'clf__n_neighbors': [5, 8, 10, 12, 15]}
+    grid = GridSearchCV(estimator=pipe, param_grid=params, scoring=based_on, cv=3)
     grid.fit(X_train, y_train)
-    print("Score based on", scoret)
-    print('best training score', grid.best_score_)
-    print('best testing score', grid.score(X_test, y_test))
-    print('best params', grid.best_params_)
-    print(' ')
+    print("Score based on:", based_on)
+    print('Best training score:', grid.best_score_)
+    print('Best testing score:', grid.score(X_test, y_test))
+    print('Best parameter(s):', grid.best_params_)
+    return based_on, grid.best_score_, grid.score(X_test, y_test), grid.best_params_
 
-# nested cross-validation KNN
-gs = GridSearchCV(estimator=pipe, param_grid=params, scoring='accuracy', cv=2)
 
-scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
-print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+score_types = ['accuracy', 'roc_auc', 'f1']
+t_knc = tuple(map(gs_knc, score_types))
+
+pipeline = Pipeline([('scl', StandardScaler()), ('clf', KNeighborsClassifier())])
+parameters = {'clf__n_neighbors': [5, 8, 10, 12, 15]}
+knc_mean = get_score(pipeline, parameters)
+
 
 # learning curve
+def lcurve(name, clf):
+    pipe = Pipeline([('pca', PCA(n_components=5)), ('clf', clf)])
+    train_sizes, train_scores, test_scores = \
+        learning_curve(estimator=pipe, X=X_train, y=y_train,
+                       train_sizes=np.linspace(0.1, 1.0, 10, 100), cv=10, n_jobs=1)
+
+    pipe.fit(X_train, y_train)
+    score = pipe.score(X_test, y_test)
+    print(f"Algo: {name:<20s} and Score: {score:0.4f}")
+    return name, score
+
 
 for name, clf in zip(names, classifiers):
     pipe = Pipeline([('pca', PCA(n_components=5)), ('clf', clf)])
