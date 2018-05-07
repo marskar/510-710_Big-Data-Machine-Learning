6b6293775dd12053fc090a2a1587589659c91e2f
Martin Skarzynski <marskar@gmail.com>
2018-05-07 19:06:46 -0400

    added plt.show at the end

--- SkillCraft.py
+++ SkillCraft.py
@@ -7,14 +7,27 @@ from sklearn.base import ClassifierMixin
 from sklearn.externals import six
 from sklearn.base import clone
 from sklearn.pipeline import _name_estimators
-import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from itertools import product
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from sklearn.decomposition import PCA
+import numpy as np
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+import warnings
+warnings.filterwarnings("ignore")
+
 
 # %% define classifier
-
-
 class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
     """ A majority vote ensemble classifier
 
@@ -92,7 +105,7 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
         ----------
         maj_vote : array-like, shape = [n_samples]
             Predicted class labels.
-            
+
         """
         if self.vote == 'probability':
             maj_vote = np.argmax(self.predict_proba(X), axis=1)
@@ -150,8 +163,7 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
             return out
 
 
-#  Using the majority voting principle to make predictions
-
+# Using the majority voting principle to make predictions
 # %% import data
 df = pd.read_csv('Skill.csv')
 
@@ -160,142 +172,67 @@ print(df.describe())
 df2 = df[df['TotalHours'] < 5000]
 df2.describe()
 
-# %% explore data
-X, y = df2.iloc[:, 1:], df2.iloc[:, 0]
+# %% define X and y global variables
+X, Y = df2.iloc[:, 1:], df2.iloc[:, 0]
 
-# In[57]:
-
-
-from sklearn.decomposition import PCA
-
+# %% PCA
 pca = PCA(n_components=2)
-
-# In[72]:
-
-
-X, y = df2.iloc[:, 1:], df2.iloc[:, 0]
 X = StandardScaler().fit_transform(X)
-pca = PCA(n_components=2)
 X = pca.fit_transform(X)
-
 h = 0.02
-
 x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
 y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
 
-cm = plt.cm.RdBu
 cm_bright = ListedColormap(['#FF0000', '#0000FF'])
 
 fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(1, 1, 1)
 
 # Plot the training points
-ax.scatter(X[:, 0], X[:, 1], c=y, cmap=cm_bright, alpha=0.5)
-
-# ax.set_xlim(xx.min(), xx.max())
-# ax.set_ylim(yy.min(), yy.max())
-# ax.set_xticks(())
-# ax.set_yticks(())
+ax.scatter(X[:, 0], X[:, 1], c=Y, cmap=cm_bright, alpha=0.5)
 
 ax.set_xlabel('Principal Component 1', fontsize=15)
 ax.set_ylabel('Principal Component 2', fontsize=15)
 ax.set_title('2 component PCA', fontsize=20)
 ax.grid()
 
-# In[59]:
-
-
-X, y = df2.iloc[:, 1:], df2.iloc[:, 0]
-# le = LabelEncoder()
-# y = le.fit_transform(y)
-
-print(y.sum() / y.size)
-print(1 - y.sum() / y.size)
-# print(df2.describe())
-
-X_train, X_test, y_train, y_test = train_test_split(X, y,
+# %% train test split
+print(Y.sum() / Y.size)
+print(1 - Y.sum() / Y.size)
+X_train, X_test, y_train, y_test = train_test_split(X, Y,
                                                     test_size=0.25,
                                                     random_state=1,
-                                                    stratify=y)
+                                                    stratify=Y)
 
-# In[60]:
-
-
-import numpy as np
-from sklearn.linear_model import LogisticRegression, Perceptron
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.neural_network import MLPClassifier
-from sklearn.naive_bayes import GaussianNB
-
-import warnings
-
-warnings.filterwarnings("ignore")
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import cross_val_score
-
-clf1 = LogisticRegression(penalty='l2',
-                          C=0.001,
-                          random_state=1)
-
-clf2 = DecisionTreeClassifier(max_depth=1,
-                              criterion='entropy',
-                              random_state=0)
-
-clf3 = KNeighborsClassifier(n_neighbors=1,
-                            p=2,
-                            metric='minkowski')
-
+# %% define classifiers and pipes
+clf1 = LogisticRegression(penalty='l2', C=0.001, random_state=1)
+clf2 = DecisionTreeClassifier(max_depth=1, criterion='entropy', random_state=0)
+clf3 = KNeighborsClassifier(n_neighbors=1, p=2, metric='minkowski')
 clf4 = Perceptron()
-
 clf5 = SVC(kernel='rbf', gamma=2, C=1)
-
 clf6 = MLPClassifier(hidden_layer_sizes=(50, 25), alpha=1)
-
 clf7 = GaussianNB()
 
-#### add 2 more classifiers here ####
-
-pipe1 = Pipeline([['sc', StandardScaler()],
-                  ['clf', clf1]])
-
-### note: no StandardScaler() here 
-
-pipe3 = Pipeline([['sc', StandardScaler()],
-                  ['clf', clf3]])
-
-pipe4 = Pipeline([['sc', StandardScaler()],
-                  ['clf', clf4]])
-
-pipe5 = Pipeline([['sc', StandardScaler()],
-                  ['clf', clf5]])
-
-pipe6 = Pipeline([['sc', StandardScaler()],
-                  ['clf', clf6]])
-
-pipe7 = Pipeline([['sc', StandardScaler()],
-                  ['clf', clf7]])
+pipe1 = Pipeline([['sc', StandardScaler()], ['clf', clf1]])
+pipe3 = Pipeline([['sc', StandardScaler()], ['clf', clf3]])
+pipe4 = Pipeline([['sc', StandardScaler()], ['clf', clf4]])
+pipe5 = Pipeline([['sc', StandardScaler()], ['clf', clf5]])
+pipe6 = Pipeline([['sc', StandardScaler()], ['clf', clf6]])
+pipe7 = Pipeline([['sc', StandardScaler()], ['clf', clf7]])
 
 clf_labels = ['Logistic regression', 'Decision tree', 'KNN', 'Perceptron', 'RBF SVC', 'Neural Network', 'Naive Bayes']
 
-# In[75]:
-
-
-# Majority Rule (hard) Voting
-
-#### fix here
+# %% Majority Rule (hard) Voting
 mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3, pipe4, pipe5, pipe6, pipe7])
 
 clf_labels += ['Majority voting']
 
-#### fix here
 all_clf = [pipe1, clf2, pipe3, pipe4, pipe5, pipe6, pipe7, mv_clf]
 
-for clf, label in zip(all_clf, clf_labels):
-    scores = cross_val_score(estimator=clf,
+for CLF, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=CLF,
                              X=X_train,
                              y=y_train,
                              cv=10,
@@ -303,19 +240,12 @@ for clf, label in zip(all_clf, clf_labels):
     print("ROC AUC: %0.2f (+/- %0.2f) [%s]"
           % (scores.mean(), scores.std(), label))
 
-# In[76]:
-
-
-# Accuracy
-
-#### fix here
+# %% Accuracy
 mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3, pipe4, pipe5, pipe6, pipe7])
-
-#### fix here
 all_clf = [pipe1, clf2, pipe3, pipe4, pipe5, pipe6, pipe7, mv_clf]
 
-for clf, label in zip(all_clf, clf_labels):
-    scores = cross_val_score(estimator=clf,
+for CLF, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=CLF,
                              X=X_train,
                              y=y_train,
                              cv=10,
@@ -323,19 +253,13 @@ for clf, label in zip(all_clf, clf_labels):
     print("Accuracy: %0.2f (+/- %0.2f) [%s]"
           % (scores.mean(), scores.std(), label))
 
-# In[77]:
-
-
-# Prune
-
-#### fix here
+# %% prune1
 mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe4, pipe6, pipe7])
 
-#### fix here
 all_clf = [pipe1, clf2, pipe3, pipe4, pipe5, pipe6, pipe7, mv_clf]
 
-for clf, label in zip(all_clf, clf_labels):
-    scores = cross_val_score(estimator=clf,
+for CLF, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=CLF,
                              X=X_train,
                              y=y_train,
                              cv=10,
@@ -343,19 +267,12 @@ for clf, label in zip(all_clf, clf_labels):
     print("ROC AUC: %0.2f (+/- %0.2f) [%s]"
           % (scores.mean(), scores.std(), label))
 
-# In[78]:
-
-
-# Prune
-
-#### fix here
+# %% prune2
 mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe4, pipe6, pipe7])
-
-#### fix here
 all_clf = [pipe1, clf2, pipe3, pipe4, pipe5, pipe6, pipe7, mv_clf]
 
-for clf, label in zip(all_clf, clf_labels):
-    scores = cross_val_score(estimator=clf,
+for CLF, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=CLF,
                              X=X_train,
                              y=y_train,
                              cv=10,
@@ -363,30 +280,20 @@ for clf, label in zip(all_clf, clf_labels):
     print("Accuracy: %0.2f (+/- %0.2f) [%s]"
           % (scores.mean(), scores.std(), label))
 
-# <br>
-# <br>
-
-# # Evaluating and tuning the ensemble classifier
-
-# In[64]:
-
-
-from sklearn.metrics import roc_curve
-from sklearn.metrics import auc
-
+# %% Plotting decision boundaries first two principal components
+# Evaluating and tuning the ensemble classifier
 mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe4, pipe6, pipe7])
 
-colors = ['black', 'orange', 'blue', 'green', 'red', 'yellow', 'purple', 'pink']  ## <- fix here
-linestyles = [':', '--', '-.', '-', ':', '--', '-.', '-']  ## <- fix here
-for clf, label, clr, ls in zip(all_clf,
-                               clf_labels, colors, linestyles):
+colors = ['black', 'orange', 'blue', 'green', 'red', 'yellow', 'purple', 'pink']
+linestyles = [':', '--', '-.', '-', ':', '--', '-.', '-']
+for CLF, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
 
     # assuming the label of the positive class is 1
-    if hasattr(clf, "predict_proba"):
-        y_pred = clf.fit(X_train,
+    if hasattr(CLF, "predict_proba"):
+        y_pred = CLF.fit(X_train,
                          y_train).predict_proba(X_test)[:, 1]
     else:
-        y_pred = clf.fit(X_train,
+        y_pred = CLF.fit(X_train,
                          y_train).predict(X_test)
 
     fpr, tpr, thresholds = roc_curve(y_true=y_test,
@@ -421,8 +328,6 @@ X_train_std = sc.fit_transform(X_train.iloc[:, [2, 3]])
 # In[66]:
 
 
-from itertools import product
-
 all_clf = [pipe1, clf2, pipe3, pipe4, pipe5, pipe6, pipe7, mv_clf]
 
 x_min = X_train_std[:, 0].min() - 1
@@ -438,11 +343,11 @@ f, axarr = plt.subplots(nrows=4, ncols=2,
                         sharey='row',
                         figsize=(14, 12))
 
-for idx, clf, tt in zip(product([0, 1, 2, 3], [0, 1]),
+for idx, CLF, tt in zip(product([0, 1, 2, 3], [0, 1]),
                         all_clf, clf_labels):
-    clf.fit(X_train_std, y_train)
+    CLF.fit(X_train_std, y_train)
 
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = CLF.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
 
     axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
@@ -475,18 +380,13 @@ plt.text(-14, 20,
 plt.show()
 
 # In[67]:
-
-
-from itertools import product
-
 pca = PCA(n_components=2)
 
 X = pca.fit_transform(X)
 X = StandardScaler().fit_transform(X)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=42)
 
 h = .02
-cm = plt.cm.RdBu
 cm_bright = ListedColormap(['#FF0000', '#0000FF'])
 
 x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
@@ -500,24 +400,24 @@ f, axarr = plt.subplots(nrows=4, ncols=2,
                         sharey='row',
                         figsize=(14, 12))
 
-for idx, clf, name in zip(product([0, 1, 2, 3], [0, 1]),
+for idx, CLF, Name in zip(product([0, 1, 2, 3], [0, 1]),
                           all_clf, clf_labels):
     # iterate over classifiers
-    # for name, clf in zip(clf_labels, all_clf):
+    # for Name, clf in zip(clf_labels, all_clf):
     # ax = plt.subplot(1, len(all_clf), i)
-    clf.fit(X_train, y_train)
-    score = clf.score(X_test, y_test)
+    CLF.fit(X_train, y_train)
+    score = CLF.score(X_test, y_test)
 
     # Plot the decision boundary. For that, we will assign a color to each
     # point in the mesh [x_min, x_max]x[y_min, y_max].
-    if hasattr(clf, "decision_function"):
-        Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    if hasattr(CLF, "decision_function"):
+        Z = CLF.decision_function(np.c_[xx.ravel(), yy.ravel()])
     else:
-        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+        Z = CLF.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
 
         # Put the result into a color plot
     Z = Z.reshape(xx.shape)
-    axarr[idx[0], idx[1]].contourf(xx, yy, Z, cmap=cm, alpha=.8)
+    axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=.8)
 
     # Plot also the training points
     axarr[idx[0], idx[1]].scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
@@ -529,10 +429,9 @@ for idx, clf, name in zip(product([0, 1, 2, 3], [0, 1]),
     axarr[idx[0], idx[1]].set_ylim(yy.min(), yy.max())
     axarr[idx[0], idx[1]].set_xticks(())
     axarr[idx[0], idx[1]].set_yticks(())
-    axarr[idx[0], idx[1]].set_title(name)
+    axarr[idx[0], idx[1]].set_title(Name)
     # if ds_cnt == 0:
-    # ax.set_title(name)
+    # ax.set_title(Name)
     axarr[idx[0], idx[1]].text(xx.max() - .3, yy.min() + .3, ('%.3f' % score).lstrip('0'),
                                size=15, horizontalalignment='right')
     i += 1
-plt.show()
